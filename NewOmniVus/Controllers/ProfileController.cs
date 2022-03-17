8961412ae05a7b55626c09d3ad805ab0c754a046
Kristoffer Nowén <kristoffer.nowen@learnet.se>
2022-03-17 19:06:52 +0100

    profile firstname updated

---
+++ NewOmniVus/Controllers/ProfileController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NewOmniVus.Data;
+using NewOmniVus.Models.Profiles;
+
+namespace NewOmniVus.Controllers
+{
+    
+    public class ProfileController : Controller
+    {
+        private readonly SecondDbContext _secondDbContext;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public ProfileController(SecondDbContext secondDbContext, SignInManager<IdentityUser> signInManager)
+        {
+            _secondDbContext = secondDbContext;
+            _signInManager = signInManager;
+        }
+
+        public async Task<IActionResult> Index( string returnUrl = null)
+        {
+
+            var model = await _secondDbContext.Profiles.SingleOrDefaultAsync(x => x.UserEmail == User.Identity.Name);
+
+            var editModel = new EditAppUserProfile
+            {
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+            };
+
+            if (returnUrl != null)
+                editModel.ReturnUrl = returnUrl;
+            else
+            {
+                editModel.ReturnUrl = "/";
+            }
+
+            return View(editModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(EditAppUserProfile profileModel, string returnUrl = null)
+        {
+            var originalProfile =
+                await _secondDbContext.Profiles.FirstOrDefaultAsync(x => x.UserEmail == User.Identity.Name);
+            
+
+            originalProfile.FirstName = profileModel.FirstName;
+            originalProfile.LastName = profileModel.LastName;
+
+            _secondDbContext.Entry(originalProfile).State = EntityState.Modified;
+            await _secondDbContext.SaveChangesAsync();
+
+            if (returnUrl != null)
+                profileModel.ReturnUrl = returnUrl;
+            else
+            {
+                profileModel.ReturnUrl = "/";
+            }
+
+            return View(profileModel);
+        }
+    }
+}
---
+++ NewOmniVus/Models/Profiles/EditAppUserProfile.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NewOmniVus.Models.Profiles
+{
+    public class EditAppUserProfile
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        [Display(Name = "E-Postadress")]
+        
+        [EmailAddress(ErrorMessage = "E-postadressen måste vara giltig")]
+        public string Email { get; set; }
+
+        [Display(Name = "Lösenord")]
+        
+        [StringLength(256, ErrorMessage = "Lösenordet måste bestå av minst 8 tecken", MinimumLength = 8)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Bekräfta Lösenord")]
+        
+        [Compare("Password", ErrorMessage = "Lösenorden matchar inte")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+
+        public string ReturnUrl { get; set; } 
+
+        public string AddressLine { get; set; }
+        public string PostalCode { get; set; }
+        public string City { get; set; }
+    }
+}
